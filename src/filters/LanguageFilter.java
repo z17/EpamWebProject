b835ce7f087b5d87a3f94e9969e08b45df74f625
z17 <admin@blweb.ru>
2016-01-27 19:48:13 +0300

    change the way to change language

--- src/filters/LanguageFilter.java
+++ src/filters/LanguageFilter.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+/**
+ * Фильтр для смены языка, анализирует url запроса, если видит get параметр lang - меняет на соответствущий язык или RU по-умолчанию.
+ */
 @WebFilter(urlPatterns = "*")
 public class LanguageFilter implements Filter {
     private static final Logger LOG = Logger.getLogger(LanguageFilter.class);
@@ -18,37 +21,26 @@ public class LanguageFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String queryString = ((HttpServletRequest)servletRequest).getQueryString();
         HttpSession session = ((HttpServletRequest) servletRequest).getSession(true);
-        boolean setLanguage = false;
-
-        if (queryString != null) {
-            String[] params = queryString.split("&");
-            for(String param: params) {
-                String[] paramValues = param.split("=");
-                if (paramValues.length == 2 && paramValues[0].equals("lang")) {
-                    Languages lang;
-                    switch (paramValues[1]) {
-                        case "en":
-                            lang = Languages.EN;
-                            break;
-                        case "ru":
-                            lang = Languages.RU;
-                            break;
-                        default:
-                            LOG.warn("try to change invalid language " + paramValues[1]);
-                            lang = Languages.RU;
-                            break;
-                    }
-                    LOG.info("change language to " + lang);
-                    // todo: заменить language на константу тут и в теге
-                    session.setAttribute("language", lang);
-                    setLanguage = true;
-                }
+        String langParam = servletRequest.getParameter("lang");
+        if (langParam != null) {
+            Languages lang;
+            switch (langParam) {
+                case "en":
+                    lang = Languages.EN;
+                    break;
+                case "ru":
+                    lang = Languages.RU;
+                    break;
+                default:
+                    LOG.warn("try to change invalid language " + langParam);
+                    lang = Languages.RU;
+                    break;
             }
-        }
-
-        if (!setLanguage) {
+            LOG.info("change language to " + lang);
+            // todo: заменить language на константу тут и в теге
+            session.setAttribute("language", lang);
+        } else {
             Languages currentLang = (Languages) session.getAttribute("language");
             if (currentLang == null) {
                 LOG.info("set default language");
